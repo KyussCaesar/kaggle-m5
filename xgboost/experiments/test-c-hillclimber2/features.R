af93c6971d11959d633459a493750110b50d4e25
Antony Southworth <southworthy@gmail.com>
2020-06-24 21:48:52 +1200

    wip

---
+++ xgboost/experiments/test-c-hillclimber2/features.R
@@ -0,0 +1,13 @@
+# features to build DM
+# should define a variable called `features` which is just a character
+# vector specifying the features to build.
+
+features =
+  c(
+    "dates",
+    "days_since_launch",
+    "tgt_volume_sum_yoy_1",
+    "tgt_volume_sum_arimaw_2",
+    "tgt_volume_sum_rollmaxr_7",
+    "tgt_volume_sum_rollrmsr_14"
+  )
