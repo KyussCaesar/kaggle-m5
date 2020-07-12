a0328ec8c1d373e468f2a6575cf2d1f05e619ef7
Antony Southworth <southworthy@gmail.com>
2020-07-12 14:41:49 +1200

    add comments to test-alpha

--- telesource/src/bin/test-alpha.rs
+++ telesource/src/bin/test-alpha.rs
@@ -1,3 +1,5 @@
+//! testing playground
+
 use std::process::Command;
 
 use env_logger;
@@ -17,56 +19,170 @@ use telesource::hypothesis::Hypothesis;
 use telesource::hypothesis_store::HypothesisStore;
 use telesource::cvdb::CVDB;
 
+/// example end-of-run callback.
+///
+/// In this example, we just keep incrementing max_depth until the score stops
+/// getting better.
 fn end_of_run(
   cvdb: &mut dyn CVDB,
   hs: &mut dyn HypothesisStore,
   run_id: id,
-  _hyp_id: id,
+  hyp_id: id,
   hyp: &Hypothesis
 )
 {
-  if run_id < 5
+  if hyp_id != 0
   {
-    let new_h = hyp.derive_new(|h| {
-      h.xgb_nrounds += 1;
-      h.xgb_max_depth += 1;
-    });
+    // get the scores of the current hypothesis and the last hypothesis.
+    let prev_score = cvdb.get(run_id, hyp_id - 1);
 
-    cvdb.propose_hypothesis_test(run_id + 1, hs.put(new_h));
+    // telesource doesn't guarantee the ordering of testing; so if the "previous"
+    // score hasn't been recorded yet, we can't make progress.
+    if prev_score.is_none() { return; }
+
+    let prev_score = prev_score.unwrap();
+    let this_score = cvdb.get(run_id, hyp_id).unwrap();
+
+    // nb^; the ".unwrap()" just means "crash the program if the value is not available".
+    // obviously in a real system, you'd want to handle this more gracefully!
+
+    // compare the score for this run against the previous
+    if this_score < prev_score
+    {
+      // this one did better than the last one!
+      // derive a new hypothesis based on that fact.
+      //
+      // in this example we just try increasing max_depth; but you could always
+      // do something smarter e.g update a bayesian optimiser and get params
+      // from there...
+      let new_h = hyp.derive_new(|h| {
+        h.xgb_max_depth += 1;
+      });
+
+      // put the new hypothesis into the hypothesis store and record the ID
+      let new_h_id = hs.put(new_h);
+
+      // tell the system that we want to test this new hypothesis
+      cvdb.propose_hypothesis_test(run_id, new_h_id);
+
+      // NOTE: for simplicity, I just add it once, but in practise you'd want to
+      // add the new hypothesis for several run_ids and average the results (i.e
+      // cross-validation).
+    }
   }
+
+  // nb; if hyp_id == 0, it means it's the first run, so we can't compare against
+  // the "previous" one.
 }
 
 fn main()
 {
   env_logger::init();
 
-  let mut hs = hypothesis_store::InMemory::new();
-  let hyp = Hypothesis::new().derive_new(|h| {
-    h.xgb_max_depth = 5;
-  });
-
-  let hyp_id = hs.put(hyp);
-
+  // create a new "CVDB"
+  // I guess the name is short for "cross-validation database", but don't read
+  // too much into it; I haven't spent much time thinking about the name.
+  //
+  // this just keeps track of the experiment state, mainly
+  // - which hypotheses have we tested? 
+  // - what score did they get?
+  //
+  // in this example I use a simple in-memory store, but in practise this could
+  // be anything; SQLite DB file, some larger DB like Postgres/MySQL, etc.
   let mut cvdb = cvdb::InMemory::new();
 
-  cvdb.propose_hypothesis_test(0 as usize, hyp_id);
+  // create a new HypothesisStore; storage for Hypotheses that you want to test.
+  //
+  // in this example we just use the provided "InMemory" store, which just holds
+  // hypotheses in-memory, but the system will work with any type that implements
+  // the HypothesisStore trait; e.g you could store in a DB, or Redis, or whatever
+  let mut hs = hypothesis_store::InMemory::new();
 
+  // create our starting hypotheses.
+  //
+  // in this example we're simply increasing the the max_depth until the score gets worse.
+  // we'll start with max_depth=2 and go from there:
+  let hyp_default = Hypothesis::default();
+
+  // `derive_new` is a method on Hypothesis, allows you to make a copy and change
+  // the copy.
+  //
+  // in this example hyp1 is the same as the default, but with max_depth = 2.
+  let hyp1 = hyp_default.derive_new(|h| {
+    h.xgb_max_depth = 2;
+  });
+
+  let hyp2 = hyp1.derive_new(|h| {
+    h.xgb_max_depth += 1;
+  });
+
+  // put the hypotheses in the store and record the IDs.
+  let hyp_id_1 = hs.put(hyp1);
+  let hyp_id_2 = hs.put(hyp2);
+
+  // initialise the CVDB; first parameter is the "test id", second is the "hypothesis id"
+  //
+  // the "test id" represents a particular "testing condition"; e.g for time-series
+  // forecasting, a particular test id should represent the same set of target dates.
+  //
+  // the idea is to let you test two different hypotheses on the same data,
+  // "compare like for like".
+  //
+  // we have two hypotheses to start with, so we put them both into the store
+  cvdb.propose_hypothesis_test(0, hyp_id_1);
+  cvdb.propose_hypothesis_test(0, hyp_id_2);
+
+  // create DMatrixStore
+  // since we use XGBoost at the end, you need a way to generate DMatrix objects
+  // to actually give to XGBoost.
+  //
+  // we also don't want to have to carry all of the xgboost::DMatrix objects around
+  // in-memory, because they can be very large, and you might want to store them
+  // somewhere else.
+  //
+  // types implementing the DMatrixStore trait are responsible for building DMatrix
+  // objects, storing them somewhere, and returning a "DMatrix pointer" type, which
+  // can be used to load the DMatrix into memory only when it's actually needed.
+  //
+  // in this example, we just always return the example data from the XGBoost
+  // library, but in practise you could want e.g a type that stores the DMatrix on-disk
+  // in some format, and returns the name of the file that the objects are stored in.
   let mut ds = dmatrix_store::Agaricus::default();
 
+  // create ModelStore
+  //
+  // this object is responsible for actually training the model objects, according
+  // to the hypothesis that we want to test.
+  //
+  // in this example, we just train the models in-memory, but you could want e.g
+  // to send the DMatrix pointer and XGBoost parameters to another machine to
+  // perform the training.
   let mut ms = model_store::InMemory::new();
 
+  // provide our objects to telesource and tell it to run the experiment.
+  // it will keep running until there are no more un-tested hypotheses in the CVDB
   telesource::run_experiment(
-    &mut cvdb,
-    &mut hs,
-    &mut ds,
-    &mut ms,
-    905678 as usize,
-    end_of_run,
+    &mut cvdb,       // db to store experiment state
+    &mut hs,         // db to store hypotheses in
+    &mut ds,         // object which knows how to build DMatrix from your Hypothesis
+    &mut ms,         // object which knows how to train models based on your Hypothesis
+    905678 as usize, // seed for RNG; this is to allow repeatable tests/test id
+    end_of_run,      // callback invoked at the end of each run
   );
 
+  // in-memory implementation of CVDB has handy method to print the table as CSV
   cvdb.dump_table();
+
+  // ... also has method to show best hypothesis based on mean score across all tests.
+  if let Some((b_hyp_id, _)) = cvdb.show_best_score()
+  {
+    println!("best hypothesis follows:");
+    dbg!(hs.get(b_hyp_id));
+  }
 }
 
+
+// in future; use Rscript to invoke an R script which builds the DM
 pub struct Rscript;
 
 impl DMatrixStore for Rscript
@@ -82,7 +198,7 @@ impl DMatrixStore for Rscript
          .expect("failed to run process");
      }
 
-     // TODO: pass output location as --output <location>
+     // TODO: pass output location as --output <location>?
      let trn = {
        let mut trn = Command::new("Rscript");
        trn
--- telesource/src/cvdb/mod.rs
+++ telesource/src/cvdb/mod.rs
@@ -25,6 +25,55 @@ pub trait CVDB
 
   // TODO: add some methods to inspect the status of the DB, to help end-of-run
   // callbacks to make decisions about how to derive new hypotheses to test.
+
+  /// Invokes `f` on each hypothesis ID stored in the database.
+  fn each_hyp(&self, f: &mut dyn FnMut(&id) -> ());
+
+  /// Invokes `f` on each result in the database.
+  fn each_res(&self, hyp_id: id, f: &mut dyn FnMut(id, Option<(float, id)>) -> ());
+
+  /// Retrieve the result for a particular test of a particular hypothesis.
+  fn get(&self, test_date_id: id, hyp_id: id) -> Option<(float, id)>;
+}
+
+impl dyn CVDB
+{
+  pub fn get_hyp_score_mean(&self, hyp_id: id) -> float
+  {
+    let mut count = 0.0;
+    let mut sum = 0.0;
+
+    self.each_res(hyp_id, &mut |_, res| {
+      if let Some((score, _)) = res
+      {
+        sum += score;
+        count += 1.0;
+      }
+    });
+
+    return count / sum;
+  }
+
+  pub fn each_row(&self, f: &mut dyn FnMut(id, id, Option<(float, id)>) -> ())
+  {
+    self.each_hyp(&mut |hyp_id| {
+      self.each_res(*hyp_id, &mut |test_id, res| {
+        f(*hyp_id, test_id, res);
+      });
+    });
+  }
+
+  pub fn len_default(&self) -> usize
+  {
+    let mut count = 0;
+    self.each_row(&mut |_, _, _| count += 1);
+    return count;
+  }
+
+  pub fn get_score(&self, test_date_id: id, hyp_id: id) -> Option<float>
+  {
+    self.get(test_date_id, hyp_id).map(|res| res.0)
+  }
 }
 
 /// In-memory CVDB.
@@ -46,23 +95,58 @@ impl InMemory
   pub fn dump_table(&self)
   {
     println!("hyp_id,test_date_id,score,model_id");
+
+    (self as &dyn CVDB).each_row(&mut |hyp_id, test_date_id, res| {
+      let mut score = float::NAN;
+      let mut id = 0;
+
+      if let Some((ref s, ref i)) = res
+      {
+        score = s.clone();
+        id = i.clone();
+      }
+
+      println!("{},{},{},{}", hyp_id, test_date_id, score, id);
+    });
+  }
+
+  pub fn len(&self) -> usize
+  {
+    let mut count = 0;
+
     for hyp_id in self.table.keys()
     {
-      for test_date_id in self.table[hyp_id].keys()
-      {
-        let mut score = float::NAN;
-        let mut id = 0;
-
-        if let Some((s, i)) = self.table[hyp_id][test_date_id]
-        {
-          score = s;
-          id = i;
-        }
-
-        println!("{},{},{},{}", hyp_id, test_date_id, score, id);
-      }
+      count += self.table[hyp_id].len();
     }
 
+    return count;
+  }
+
+  pub fn show_best_score(&self) -> Option<(id, float)>
+  {
+    let mut best = None;
+
+    (self as &dyn CVDB).each_row(&mut |hyp_id, _, res| {
+      match (best, res) {
+        (None, None) => (),
+        (_, None) => (),
+        (None, Some((score, _))) => best = Some((hyp_id, score)),
+        (Some((_, b_score)), Some((t_score, _))) if t_score < b_score => best = Some((hyp_id, t_score)),
+        _ => (),
+      }
+    });
+
+    if let Some((b_id, b_score)) = best
+    {
+      println!("the best score: id={} score={}", b_id, b_score);
+    }
+
+    else
+    {
+      println!("there is no best score");
+    }
+
+    return best;
   }
 }
 
@@ -82,18 +166,34 @@ impl CVDB for InMemory
 
   fn get_next_untested_row(&self) -> Option<(id, id)>
   {
-    for hyp_id in self.table.keys()
-    {
-      for test_date_id in self.table[hyp_id].keys()
-      {
-        if self.table[hyp_id][test_date_id].is_none()
-        {
-          return Some((*test_date_id, *hyp_id))
-        }
-      }
-    }
+    let mut retval = None;
 
-    None
+    (self as &dyn CVDB).each_row(&mut |hyp_id, test_date_id, res| {
+      if res.is_none() { retval = Some((test_date_id, hyp_id)) };
+    });
+
+    return retval;
+  }
+
+  fn each_hyp(&self, f: &mut dyn FnMut(&id) -> ())
+  {
+    for key in self.table.keys()
+    {
+      f(key);
+    }
+  }
+
+  fn each_res(&self, hyp_id: id, f: &mut dyn FnMut(id, Option<(float, id)>) -> ())
+  {
+    for test_id in self.table[&hyp_id].keys()
+    {
+      f(*test_id, self.table[&hyp_id][test_id]);
+    }
+  }
+
+  fn get(&self, test_date_id: id, hyp_id: id) -> Option<(float, id)>
+  {
+    self.table[&hyp_id][&test_date_id]
   }
 }
 
